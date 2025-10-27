e27ec69e3067506cb73a338d8434cc142b2f4cf0
somesrandomdev <ibrahimakhaliloullahndiaye@gmail.com>
2025-10-27 11:18:00 +0000

    fixed npm issue

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
 module.exports = app;
